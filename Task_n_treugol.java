2d396e616c28f2e4deab76e39bf390a120ffd506
Victoriia Silaeva <Djessi1996@mail.ru>
2022-06-19 20:57:54 +0300

    n_treug

---
+++ Task_n_treugol.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task_n_treugol {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = in.nextInt();
+        int T = (n * (n + 1)) / 2;
+        System.out.println(T);
+    }
+}
